101b50ce8969e2efbee96cfa53c3f2efc322d306
Fatima <skfatimaak4532@gmail.com>
2023-10-05 16:18:04 +0530

    modified

--- python patterns/pattern4.py
+++ python patterns/pattern4.py
@@ -1,6 +1,6 @@
-rows = 5
+rows = int(input("Enter the no.of rows: "))
 b = 0
-# reverse for loop from 5 to 0
+# reverse for loop from no.of rows to 0
 for i in range(rows, 0, -1):
     b += 1
     for j in range(1, i + 1):
